9e2757387e61edaf619d5d6d75f87bcd7e6f5a85
Histman7 <hsandovalt1900@alumno.ipn.mx>
2024-03-05 21:11:19 -0600

    Final

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ let contenedorPuntosPC = document.getElementById('puntos-computador')
 let mensajeElement = document.getElementById('mensaje')
 let contenedorGanaPunto = document.getElementById('gana-punto')
 
-let eleccionUsuario = document.getElementById('eleccion-usuario')
-let eleccionPC = document.getElementById('eleccion-computadora')
-let elegiArma = document.getElementById('elegi-tuarma')
+let eleccionUsuarioElement = document.getElementById('eleccion-usuario')
+let eleccionPCElement = document.getElementById('eleccion-computadora')
+let elegiArmaElement = document.getElementById('elegi-tuarma')
 
 let btnArmas = document.querySelectorAll('.arma')
+let btnReiniciar = document.getElementById('reiniciar')
 
 btnArmas.forEach(btn => {
   btn.addEventListener('click', e=>{
@@ -25,5 +26,65 @@ btnArmas.forEach(btn => {
     
     console.log(eleccionPC)
     console.log(eleccionUsuario)
+    
+    if (
+        (eleccionUsuario === 'piedra' && eleccionPC === '✂️') ||
+        (eleccionUsuario === 'tijera' && eleccionPC === '📋') ||
+        (eleccionUsuario === 'papel' && eleccionPC === '✊')
+    ){
+      ganaUsuario()
+    }else if (
+        (eleccionPC === '✊' && eleccionUsuario === 'tijera') ||
+        (eleccionPC === '✂️' && eleccionUsuario === 'papel') ||
+        (eleccionPC === '📋' && eleccionUsuario === 'piedra')
+    ){
+      ganaPC()
+    }else {
+      empate()
+    }
+    
+    mensajeElement.classList.remove('disabled')
+    eleccionUsuarioElement.innerText = eleccionUsuario
+    eleccionPCElement.innerText = eleccionPC
+    
+    if (puntosComputador === 5 || puntosUsuario === 5){
+      if (puntosUsuario === 5){
+        instruccionesElement.innerText = '🔥 ¡Ganaste! 🔥'
+      }
+      else if(puntosComputador === 5){
+        instruccionesElement.innerText = '😢 ¡Perdiste! 😢'
+      }
+      
+      elegiArmaElement.classList.add('disabled')
+      btnReiniciar.classList.remove('disabled')
+      btnReiniciar.addEventListener("click", e => {
+        e.preventDefault()
+        btnReiniciar.classList.add('disabled')
+        elegiArmaElement.classList.remove('disabled')
+        mensajeElement.classList.add('disabled')
+        puntosUsuario = 0
+        puntosComputador = 0
+        
+        contenedorPuntosUsuario.innerText = puntosUsuario
+        contenedorPuntosUsuario.innerText = puntosComputador
+        instruccionesElement.innerText = 'El primero en llegar a 5 gana'
+      })
+    }
   })
 })
+
+function ganaUsuario() {
+  puntosUsuario++
+  contenedorPuntosUsuario.innerText = puntosUsuario
+  contenedorGanaPunto.innerText = '¡Ganaste un punto! 🔥'
+}
+
+function ganaPC() {
+  puntosComputador++
+  contenedorPuntosPC.innerText = puntosComputador
+  contenedorGanaPunto.innerText = '¡La computadora ganó un punto 😢!'
+}
+
+function empate() {
+  contenedorGanaPunto.innerText = '¡Empate! 😒'
+}
